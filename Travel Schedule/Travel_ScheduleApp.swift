24143b7a5eac51395fca7ec1c4755153bf43ebfe
Sergey Kemenov <skemenov@icloud.com>
2024-03-10 08:34:47 +0300

    Initial Commit

---
+++ Travel Schedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleApp.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
