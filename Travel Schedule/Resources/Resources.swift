30a734befba780b4508dc53ec9e16b2aa762e4e2
Sergey Kemenov <skemenov@icloud.com>
2024-03-12 10:53:39 +0300

    Added Resources

---
+++ Travel Schedule/Resources/Resources.swift
@@ -0,0 +1,12 @@
+//
+//  Resources.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 10.03.2024.
+//
+
+import Foundation
+
+enum Resources {
+    static let apikey = "8ce2d934-9f3a-4607-ba91-4c508695b061"
+}
