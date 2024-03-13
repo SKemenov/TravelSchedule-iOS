2ba3064e4b3ede05f08c04d20eff58d42d8f29c6
Sergey Kemenov <skemenov@icloud.com>
2024-03-13 23:20:07 +0300

    Added `maxJsonSize` property

--- Travel Schedule/Resources/Resources.swift
+++ Travel Schedule/Resources/Resources.swift
@@ -9,4 +9,5 @@ import Foundation
 
 enum Resources {
     static let apiKey = "8ce2d934-9f3a-4607-ba91-4c508695b061"
+    static let maxJsonSize: Int = 44 * 1024 * 1024 // Размер возвращаемого JSON-документа — около 40 МБ.
 }
