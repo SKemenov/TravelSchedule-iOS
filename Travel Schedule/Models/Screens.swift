82b555febc58efed0ad9268de5222a6b09073f6b
Sergey Kemenov <skemenov@icloud.com>
2024-04-22 08:26:14 +0300

    Added `Screens` enum for NavigationStack

---
+++ Travel Schedule/Models/Screens.swift
@@ -0,0 +1,15 @@
+//
+//  Screens.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 22.04.2024.
+//
+
+import Foundation
+
+enum Screens {
+    case cityView
+    case stationView
+    case routeView
+    case agreementView
+}
