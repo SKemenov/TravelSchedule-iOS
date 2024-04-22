d74831b6afc2e19c200188db715990b3a4cf8520
Sergey Kemenov <skemenov@icloud.com>
2024-04-22 08:48:34 +0300

    Added `CGFloat` extensions

---
+++ Travel Schedule/Extensions/CGFloat+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat+Extensions.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 22.04.2024.
+//
+
+import Foundation
+
+extension CGFloat {
+    /// 17
+    static let iconSize = 17.0
+}
