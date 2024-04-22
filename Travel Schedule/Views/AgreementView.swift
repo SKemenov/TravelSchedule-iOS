db8dce9150ed5544d9612cd7bd66bd64e050ff3b
Sergey Kemenov <skemenov@icloud.com>
2024-04-22 08:32:39 +0300

    Added `AgreementView` structure prototype

---
+++ Travel Schedule/Views/AgreementView.swift
@@ -0,0 +1,18 @@
+//
+//  AgreementView.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 22.04.2024.
+//
+
+import SwiftUI
+
+struct AgreementView: View {
+    var body: some View {
+        Text("User Agreement here!")
+    }
+}
+
+#Preview {
+    AgreementView()
+}
