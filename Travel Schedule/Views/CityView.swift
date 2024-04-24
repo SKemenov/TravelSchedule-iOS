6b3cc38569c2a671147612f759572e38ff2cee21
Sergey Kemenov <skemenov@icloud.com>
2024-04-24 21:30:51 +0300

    Refactored `CityView` view prototype

--- Travel Schedule/Views/CityView.swift
+++ Travel Schedule/Views/CityView.swift
@@ -9,18 +9,18 @@ import SwiftUI
 
 struct CityView: View {
     @Binding var schedule: Schedule
-    @Binding var navPath: [Screens]
+    @Binding var navPath: [ViewsRouter]
     @Binding var direction: Int
 
     var body: some View {
         VStack {
             ForEach($schedule.cities) { $city in
-                NavigationLink(value: Screens.stationView) {
-                    RowSearchView(rowString: city.title) // Text(city.title)
+                NavigationLink(value: ViewsRouter.stationView) {
+                    RowSearchView(rowString: city.title)
                 }
                     .simultaneousGesture(TapGesture().onEnded {
                         schedule.destinations[direction].cityTitle = city.title
-                        print(#fileID, "direction code", direction, "City", schedule.destinations[direction].cityTitle)
+                        // print(#fileID, "direction code", direction, "City", schedule.destinations[direction].cityTitle)
                     })
             }
         }
