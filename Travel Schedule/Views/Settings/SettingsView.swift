cd538283acc1a1e9f93960d3449208ec07105975
Sergey Kemenov <skemenov@icloud.com>
2024-04-24 22:30:49 +0300

    Redesign `SettingsView` view

--- Travel Schedule/Views/Settings/SettingsView.swift
+++ Travel Schedule/Views/Settings/SettingsView.swift
@@ -11,16 +11,30 @@ struct SettingsView: View {
     @Binding var darkMode: Bool
 
     var body: some View {
-        VStack {
-            Toggle("Switch is here", isOn: $darkMode)
-            // NavigationLink(value: ViewsRouter.agreementView) {
+        VStack(spacing: 0) {
+            Toggle("Тёмная тема", isOn: $darkMode)
+                .font(.regMedium)
+                .padding(.horizontal, 16)
+                .frame(maxWidth: .infinity, maxHeight: 60)
+                .tint(.ypBlue)
             NavigationLink {
                 AgreementView()
             } label: {
-                RowSearchView(rowString: "User Agreement")
+                RowSearchView(rowString: "Пользовательское соглашение")
             }
+            .font(.regMedium)
+            .padding(16)
+            .frame(maxWidth: .infinity, maxHeight: 60)
             Spacer()
+            VStack(alignment: .center, spacing: 16) {
+                Text("Приложение использует API «Яндекс.Расписания»")
+                Text("Версия \(Bundle.main.appVersionLong).\(Bundle.main.appBuild)")
+            }
+            .font(.regSmall)
+            .frame(minHeight: 44)
         }
+        .padding(.vertical, 24)
+        .foregroundColor(.ypBlackDuo)
     }
 }
 
