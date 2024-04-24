09adb1bc1b804cb6241b3839c1de6633005a4d48
Sergey Kemenov <skemenov@icloud.com>
2024-04-24 21:34:39 +0300

    Added sf symbols properties

--- Travel Schedule/Extensions/Image+Extensions.swift
+++ Travel Schedule/Extensions/Image+Extensions.swift
@@ -10,15 +10,15 @@ import SwiftUI
 extension Image {
     // static let iconTabSearch = Image(systemName: "arrow.up.message.fill")
     // static let iconTabSettings = Image(systemName: "gearshape.fill")
-    static let iconTabSearch = Image("Schedule").renderingMode(.template)
-    static let iconTabSettings = Image("Settings").renderingMode(.template)
+    static let iconTabSearch = Image("schedule").renderingMode(.template)
+    static let iconTabSettings = Image("settings").renderingMode(.template)
 
     static let iconSearchSwap = Image(systemName: "arrow.2.squarepath")
     static let iconSearching = Image(systemName: "magnifyingglass")
     static let iconSearCancel = Image(systemName: "xmark.circle.fill")
     static let iconBackward = Image(systemName: "chevron.backward")
     static let iconForward = Image(systemName: "chevron.forward")
-
+    static let iconArrow = Image(systemName: "arrow.forward")
 
     static let imageServerError = Image("xmark.circle.fill")
 }
