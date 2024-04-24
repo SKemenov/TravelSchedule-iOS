4c3ff025c38c10c72863aa64012974a626c2be0c
Sergey Kemenov <skemenov@icloud.com>
2024-04-25 01:31:54 +0300

    Added `SetCustomNavBackButton` ViewModifier

--- Travel Schedule/Extensions/View+Extensions.swift
+++ Travel Schedule/Extensions/View+Extensions.swift
@@ -12,6 +12,10 @@ extension View {
         modifier(SetRowElement())
     }
 
+    func setCustomNavBackButton() -> some View {
+        modifier(SetCustomNavBackButton())
+    }
+
     func setCustomNavigationBar(title: String = "") -> some View {
         modifier(SetCustomNavigationBar(title: title))
     }
