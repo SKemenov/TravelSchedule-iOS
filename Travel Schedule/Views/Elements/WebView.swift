bd49e3016c495b75deb868590477537b4ef37c76
Sergey Kemenov <skemenov@icloud.com>
2024-04-25 09:53:16 +0300

    Added `WebView` to implement WebKit View into SwiftUI

---
+++ Travel Schedule/Views/Elements/WebView.swift
@@ -0,0 +1,22 @@
+//
+//  WebView.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 25.04.2024.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
