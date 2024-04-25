a4b01d3735447ea0784404ee8a0cf6772d397095
Sergey Kemenov <skemenov@icloud.com>
2024-04-25 10:13:30 +0300

    Added icons for custom toggle styles

--- Travel Schedule/Extensions/Image+Extensions.swift
+++ Travel Schedule/Extensions/Image+Extensions.swift
@@ -15,10 +15,17 @@ extension Image {
 
     static let iconSearchSwap = Image(systemName: "arrow.2.squarepath")
     static let iconSearching = Image(systemName: "magnifyingglass")
-    static let iconSearCancel = Image(systemName: "xmark.circle.fill")
+    static let iconSearchCancel = Image(systemName: "xmark.circle.fill")
+
     static let iconBackward = Image(systemName: "chevron.backward")
     static let iconForward = Image(systemName: "chevron.forward")
+
     static let iconArrow = Image(systemName: "arrow.forward")
+
+    static let iconCheckboxOn = Image(systemName: "checkmark.square.fill")
+    static let iconCheckboxOff = Image(systemName: "square")
+    static let iconRadioOn = Image(systemName: "largecircle.fill.circle")
+    static let iconRadioOff = Image(systemName: "circle")
 
     static let imageServerError = Image("xmark.circle.fill")
 }
