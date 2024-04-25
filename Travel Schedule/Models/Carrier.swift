e8c5e1c60e2c880ee255c0f6f8f1d74bd5407eea
Sergey Kemenov <skemenov@icloud.com>
2024-04-25 10:14:48 +0300

    Resigned `CarrierView` view

--- Travel Schedule/Models/Carrier.swift
+++ Travel Schedule/Models/Carrier.swift
@@ -11,14 +11,16 @@ struct Carrier: Hashable, Identifiable {
     var id = UUID()
     var title: String
     var logoName: String
+    var email: String
+    var phone: String
 }
 
 extension Carrier {
-    static let emptyData = Carrier(title: "", logoName: "")
+    static let emptyData = Carrier(title: "", logoName: "", email: "", phone: "")
 
     static let sampleData: [Carrier] = [
-        Carrier(title: "РЖД", logoName: "rzhd"),
-        Carrier(title: "ФГК", logoName: "fgk"),
-        Carrier(title: "Урал логистика", logoName: "ural")
+        Carrier(title: "РЖД", logoName: "rzhd", email: "i.lozgkina@yandex.ru", phone: "+7 (904) 329-27-71"),
+        Carrier(title: "ФГК", logoName: "fgk", email: "i.lozgkina@yandex.ru", phone: "+7 (904) 329-27-71"),
+        Carrier(title: "Урал логистика", logoName: "ural", email: "i.lozgkina@yandex.ru", phone: "+7 (904) 329-27-71")
     ]
 }
--- Travel Schedule/Views/Routes/CarrierView.swift
+++ Travel Schedule/Views/Routes/CarrierView.swift
@@ -9,26 +9,56 @@ import SwiftUI
 
 struct CarrierView: View {
     @State var carrier: Carrier
-    // @Binding var carrier: Carrier
+    // swiftlint:disable:next attributes
+    @Environment(\.openURL) private var openURL
 
     var body: some View {
-        VStack {
-            Text(carrier.logoName)
+        VStack(alignment: .leading, spacing: 0) {
+            Image(carrier.logoName)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: 104)
             Text("ОАО «\(carrier.title)»")
-            Text("E-mail")
-            Text("\(carrier.id)")
-            Text("Телефон")
-            Text("\(carrier.id)")
+                .font(.boldMedium)
+                .frame(maxWidth: .infinity, maxHeight: 29, alignment: .leading)
+                .padding(.vertical, 16)
+            VStack(alignment: .leading, spacing: 0) {
+                Text("E-mail")
+                    .font(.regMedium)
+                    .foregroundStyle(.ypBlackDuo)
+                Button {
+                    guard let url = URL(string: "mailto:"+carrier.email) else { return }
+                    openURL(url)
+                } label: {
+                    Text("\(carrier.email)")
+                        .font(.regSmall)
+                    .foregroundStyle(.ypBlue)
+                }
+            }
+            .frame(height: 60)
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Телефон")
+                    .font(.regMedium)
+                    .foregroundStyle(.ypBlackDuo)
+                Button {
+                    guard let url = URL(string: "tel:"+carrier.phone) else { return }
+                    openURL(url)
+                } label: {
+                    Text("\(carrier.phone)")
+                        .font(.regSmall)
+                    .foregroundStyle(.ypBlue)
+                }
+            }
+            .frame(height: 60)
             Spacer()
         }
-        .navigationTitle("ИНформация о перевозчике")
-        .navigationBarTitleDisplayMode(.inline)
+        .padding(.horizontal, 16)
+        .setCustomNavigationBar(title: "ИНформация о перевозчике")
     }
 }
 
 #Preview {
     NavigationStack {
         CarrierView(carrier: Carrier.sampleData[0])
-        // CarrierView(carrier: .constant(Carrier.sampleData[0]))
     }
 }
