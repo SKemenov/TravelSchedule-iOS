7a061070479c04f439cc859f55f9c4ac7a227e16
Sergey Kemenov <skemenov@icloud.com>
2024-04-25 10:11:32 +0300

    Added custom `RadioButton` toggleStyle

--- Travel Schedule/Models/ViewModifier/RadioButtonToggleStyle.swift
+++ Travel Schedule/Models/ViewModifier/RadioButtonToggleStyle.swift
@@ -1,5 +1,5 @@
 //
-//  CheckboxToggleStyle.swift
+//  RadioButtonToggleStyle.swift
 //  Travel Schedule
 //
 //  Created by Sergey Kemenov on 25.04.2024.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct CheckboxToggleStyle: ToggleStyle {
+struct RadioButtonToggleStyle: ToggleStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         HStack(spacing: 0) {
             configuration.label
             Spacer()
-            (configuration.isOn ? Image.iconCheckboxOn : Image.iconCheckboxOff)
+            (configuration.isOn ? Image.iconRadioOn : Image.iconRadioOff)
                 .resizable()
                 .frame(width: 24, height: 24)
                 .onTapGesture { configuration.isOn.toggle() }
