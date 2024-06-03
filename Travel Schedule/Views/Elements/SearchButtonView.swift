9fb94fc9622764bfa5288d41c6e75c1bb5b35f5d
Sergey Kemenov <skemenov@icloud.com>
2024-06-03 12:48:45 +0300

    Extracted `SearchButtonView`

---
+++ Travel Schedule/Views/Elements/SearchButtonView.swift
@@ -0,0 +1,44 @@
+//
+//  SearchButtonView.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 03.06.2024.
+//
+
+import SwiftUI
+
+struct SearchButtonView: View {
+    private let searchButtonTitle = "Найти"
+    let destinations: [Destination]
+    let route: ViewsRouter
+
+    private var isDepartureReady: Bool {
+        !destinations[.departure].cityTitle.isEmpty && !destinations[.departure].stationTitle.isEmpty
+    }
+    private var isArrivalReady: Bool {
+        !destinations[.arrival].cityTitle.isEmpty && !destinations[.arrival].stationTitle.isEmpty
+    }
+
+    var body: some View {
+        if isDepartureReady && isArrivalReady {
+            NavigationLink(value: route) {
+                Text(searchButtonTitle)
+                    .font(.boldSmall)
+                    .foregroundStyle(AppColors.Universal.white)
+                    .frame(width: AppSizes.Width.searchButton, height: AppSizes.Height.searchButton)
+                    .background(.ypBlue)
+                    .clipShape(RoundedRectangle(cornerRadius: AppSizes.CornerRadius.large))
+                    .padding(AppSizes.Spacing.large)
+            }
+        }
+    }
+
+    init(for destinations: [Destination], showView route: ViewsRouter) {
+        self.destinations = destinations
+        self.route = route
+    }
+}
+
+#Preview {
+    SearchButtonView(for: Destination.sampleData, showView: ViewsRouter.cityView)
+}
