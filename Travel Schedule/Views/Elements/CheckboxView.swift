469c52d52177bc1834613df4c7dbb2f0fd8c6c7f
Sergey Kemenov <skemenov@icloud.com>
2024-06-03 20:26:45 +0300

    Extracted `CheckboxView`

---
+++ Travel Schedule/Views/Elements/CheckboxView.swift
@@ -0,0 +1,43 @@
+//
+//  CheckboxView.swift
+//  Travel Schedule
+//
+//  Created by Sergey Kemenov on 03.06.2024.
+//
+
+import SwiftUI
+
+struct CheckboxView: View {
+    enum CheckboxType {
+        case morning, afternoon, evening, night
+
+        var title: String {
+            switch self {
+            case .morning:
+                return String(localized: "Утро 06:00 - 12:00")
+            case .afternoon:
+                return String(localized: "День 12:00 - 18:00")
+            case .evening:
+                return String(localized: "Вечер 18:00 - 00:00")
+            case .night:
+                return String(localized: "Ночь 00:00 - 06:00")
+            }
+        }
+    }
+    let type: CheckboxType
+
+    @Binding var filter: Bool
+
+    var body: some View {
+        Toggle(type.title, isOn: $filter)
+            .setRowElement()
+            .toggleStyle(.checkBox)
+    }
+}
+
+#Preview {
+    VStack {
+        CheckboxView(type: .morning, filter: .constant(Filter.fullSearch.isMorning))
+        CheckboxView(type: .morning, filter: .constant(Filter.customSearch.isMorning))
+    }
+}
