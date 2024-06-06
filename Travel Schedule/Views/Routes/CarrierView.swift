6fed49dedd2c274f2a15971a280fd206c5d49496
Sergey Kemenov <skemenov@icloud.com>
2024-06-06 21:05:56 +0300

    Refactored Carrier view

--- Travel Schedule/Views/Routes/CarrierView.swift
+++ Travel Schedule/Views/Routes/CarrierView.swift
@@ -8,54 +8,32 @@
 import SwiftUI
 
 struct CarrierView: View {
+    private let title = "ИНформация о перевозчике"
+
     @State var carrier: Carrier
-    // swiftlint:disable:next attributes
-    @Environment(\.openURL) private var openURL
+
+    private var carrierTitle: String { "ОАО «\(carrier.title)»" }
 
     var body: some View {
         VStack(alignment: .leading, spacing: .zero) {
             Image(carrier.logoName)
                 .resizable()
                 .scaledToFit()
-                .frame(maxWidth: .infinity, maxHeight: 104)
-                .background(.ypWhite)
-                .clipShape(RoundedRectangle(cornerRadius: 24))
-            Text("ОАО «\(carrier.title)»")
-                .font(.boldMedium)
-                .frame(maxWidth: .infinity, maxHeight: 29, alignment: .leading)
-                .padding(.vertical, .spacerL)
-            VStack(alignment: .leading, spacing: 0) {
-                Text("E-mail")
-                    .font(.regMedium)
-                    .foregroundStyle(.ypBlackDuo)
-                Button {
-                    guard let url = URL(string: "mailto:" + carrier.email) else { return }
-                    openURL(url)
-                } label: {
-                    Text("\(carrier.email)")
-                        .font(.regSmall)
-                    .foregroundStyle(.ypBlue)
-                }
-            }
-            .frame(height: 60)
-            VStack(alignment: .leading, spacing: 0) {
-                Text("Телефон")
-                    .font(.regMedium)
-                    .foregroundStyle(.ypBlackDuo)
-                Button {
-                    guard let url = URL(string: "tel:" + carrier.phone) else { return }
-                    openURL(url)
-                } label: {
-                    Text("\(carrier.phone)")
-                        .font(.regSmall)
-                    .foregroundStyle(.ypBlue)
-                }
-            }
-            .frame(height: 60)
+                .frame(maxWidth: .infinity, maxHeight: AppSizes.Height.carrierFullLogo)
+                .background(AppColors.Universal.white)
+                .clipShape(RoundedRectangle(cornerRadius: AppSizes.CornerRadius.xxLarge))
+            Text(carrierTitle)
+                .font(AppFonts.Bold.medium)
+                .frame(maxWidth: .infinity, maxHeight: AppSizes.Height.carrierTitle, alignment: .leading)
+                .padding(.vertical, AppSizes.Spacing.large)
+
+            CarrierContactView(carrier: carrier, type: .email)
+
+            CarrierContactView(carrier: carrier, type: .phone)
             Spacer()
         }
-        .setCustomNavigationBar(title: "ИНформация о перевозчике")
         .padding(.horizontal, AppSizes.Spacing.large)
+        .setCustomNavigationBar(title: title)
     }
 }
 
