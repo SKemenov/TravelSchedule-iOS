e01e27bb619f511bb825ea688aaa4a059e8a6ab0
Sergey Kemenov <skemenov@icloud.com>
2024-06-09 18:33:06 +0300

    Fixed animation, added 2nd TabView

--- Travel Schedule/Views/Stories/StoriesTabView.swift
+++ Travel Schedule/Views/Stories/StoriesTabView.swift
@@ -8,23 +8,22 @@
 import SwiftUI
 
 struct StoriesTabView: View {
-    // MARK: - Constants
-    private let firstPage = 0
-
     // MARK: - Properties
-    private var stories: [Story] { storiesList[initialIndex].stories }
-    private var lastPage: Int { stories.count - 1 }
-
-    @Binding var storiesList: [StoriesList]
-    @Binding var initialIndex: Int
+    @Binding var stories: [Story]
+    @Binding var storyIndex: Int
     @Binding var currentPage: Int
 
     // MARK: - View
     var body: some View {
-        TabView(selection: $currentPage) {
-            ForEach(Array(stories.enumerated()), id: \.offset) { index, story in
-                SingleStoryView(model: story)
-                    .tag(index)
+        TabView(selection: $storyIndex) {
+            ForEach(Array(stories.enumerated()), id: \.offset) { storyIndex, story in
+                TabView(selection: $currentPage) {
+                    ForEach(Array(story.storyPages.enumerated()), id: \.offset) { pageIndex, page in
+                        SingleStoryView(model: page)
+                            .tag(pageIndex)
+                    }
+                }
+                .tag(storyIndex)
             }
         }
         .ignoresSafeArea()
@@ -34,8 +33,8 @@ struct StoriesTabView: View {
 
 #Preview {
     StoriesTabView(
-        storiesList: .constant(StoriesList.mockData),
-        initialIndex: .constant(1),
+        stories: .constant(Story.mockData),
+        storyIndex: .constant(1),
         currentPage: .constant(0)
     )
 }
